453c72ceca1694c544200c831e369952002319ab
Arthur Danskin <arthurdanskin@gmail.com>
2018-07-10 21:39:00 -0400

    change name to TacticsAI

--- mod-assets/factions.lua
+++ mod-assets/factions.lua
@@ -4,8 +4,8 @@
   --   in-game values from colliding.  You probably don't need to change this.
   81 = {
     -- Faction default colors, if you feel like changing them.
-    color0=0x0302b3,
-    color1=0x000080,
+    color0=0x03f2b3,
+    color1=0x00f080,
     primaries=2,
 
     -- ((TODO))
@@ -13,7 +13,7 @@
 
     -- Faction-wide application of a particular AI.
     --   Must match a file in the "ai" folder (Visual Studio project name).
-    ainame="VanillaModAi",
+    ainame="TacticsAI",
 
     -- 0: Not a player-selectable faction (you probably don't want that).
     -- 1: Player-selectable *after* beating it in a random encounter.
@@ -21,10 +21,10 @@
     playable=2,
 
     -- Rename the mod faction as you like.
-    name=_("Vanilla AI Mod"),
+    name=_("Tactics AI Mod"),
 
     -- Must match a ship name in the "ships" folder.
     --   The numeric prefix is this faction's ID number.
-    start="80_interceptor"
+    start="81_interceptor"
   }
 }
